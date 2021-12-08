1cea5116af60370034614860aba48fcae41c23fb
Sujon <s.rabiulislam429@gmail.com>
2021-12-08 10:04:15 +0600

    initial commit 6

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const port = process.env.port || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('I Love Bangladesh!');
+});
+
+app.get('/multi/:a/:b', (req, res) => {
+    var a = parseInt(req.params.a);
+    var b = parseInt(req.params.b);
+    var c = Number(a*b)
+    res.send("The multiplication of "+ a +" and " + b + " is = " + Number(c));
+
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
--- test/test.js
+++ test/test.js
@@ -2,7 +2,13 @@ const request = require("supertest");
 const app = require("../index");
 
 describe("GET /", () => {
-    it("responds with Hello World22!", (done) => {
-        request(app).get("/").expect("Hello World!", done);
+    it("responds with I Love Bangladesh!", (done) => {
+        request(app).get("/").expect("I Love Bangladesh!", done);
+    })
+});
+
+describe("GET /multi/10/5", () => {
+    it("The multiplication of 10 and 5 is = 50", (done) => {
+        request(app).get("/multi/10/5").expect("The multiplication of 10 and 5 is = 50", done);
     })
 });
